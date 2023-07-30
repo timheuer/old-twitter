4908b10a208d6c551613a12595b9975593f552dd
Tim Heuer <tim@timheuer.com>
2023-07-30 15:12:19 -0700

    Rename post to Tweet

--- scripts/content.js
+++ scripts/content.js
@@ -23,8 +23,13 @@ function waitForElement(selector) {
 
 // Set loading page icon
 waitForElement('[aria-label="Loading…"]').then((container) => {
-  // TODO
   container.innerHTML = "";
+
+  const twitterIcon = document.createElement("img");
+  twitterIcon.src = chrome.runtime.getURL("../assets/icon128.png");
+  twitterIcon.width = 42;
+  twitterIcon.height = 42;
+  container.appendChild(twitterIcon);
 });
 
 // Set web app icon
@@ -38,3 +43,31 @@ waitForElement('[aria-label="Twitter"]').then((elm) => {
   twitterIcon.height = 42;
   container.appendChild(twitterIcon);
 });
+
+// Set tweet button
+waitForElement('[aria-label="Tweet"]').then((elm) => {
+  // from the element find any descendant that is a <span> with the innerText "Post"
+  const tweetButton = findSpanWithTweetInnerHTML(elm);
+
+  if (tweetButton) {
+    tweetButton.innerText = "Tweet";
+  }
+});
+
+function findSpanWithTweetInnerHTML(element) {
+  if (element.children.length === 0) {
+    return null;
+  }
+  for (let i = 0; i < element.children.length; i++) {
+    const child = element.children[i];
+    if (child.tagName === 'SPAN' && child.innerHTML === 'Post') {
+      return child;
+    }
+    const found = findSpanWithTweetInnerHTML(child);
+    if (found !== null) {
+      return found;
+    }
+  }
+  return null;
+}
+
